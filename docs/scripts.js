7cfeeb1e9f30dfbd273b303d7d8b8b84258d928a
XunNam <hoang.xuannam@outlook.com>
2024-10-07 21:09:52 +0700

    Basic Landing Page (NO RESPONSIVE)

---
+++ docs/scripts.js
@@ -0,0 +1,10 @@
+/*
+const buyNow = document.getElementById('buyNow');
+
+buyNow.addEventListener('click', function(){
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+*/
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./docs/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'baloo-paaji-2': ['"Baloo Paaji 2"', 'sans-serif'],
+        'playfair-display': ['"Playfair Display"', 'serif'],
+        'eb-garamond': ['"EB Garamond"', 'serif'],
+        'lato': ['"Lato"', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
